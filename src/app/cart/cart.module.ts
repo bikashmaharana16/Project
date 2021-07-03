195696869efa36626972f4c62684fc7192376008
Bikash Kumar Maharana <76558318+bikashmaharana16@users.noreply.github.com>
2021-07-03 08:14:23 +0530

    Delete src directory

--- src/app/cart/cart.module.ts
+++
@@ -1,8 +0,0 @@
-import { CartRoutingModule } from './cart.routing';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-@NgModule({
-    imports: [ CommonModule , CartRoutingModule ]
-})
-export class CartModule {}
--- src/app/order/order.module.ts
+++
@@ -1,8 +0,0 @@
-import { OrderRoutingModule } from './order.routing';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-@NgModule({
-    imports: [ CommonModule , OrderRoutingModule ]
-})
-export class OrderModule {}
